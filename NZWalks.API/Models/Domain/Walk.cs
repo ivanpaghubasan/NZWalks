14d70ce6fb4be0bddbb032ea278b470a860ab430
HS-WIN-1201\Ivan Paghubasan <ivan.paghubasan@mph-intl.com>
2026-02-18 22:08:46 +0800

    Add project files.

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid WalkId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+        public Difficulty Difficulty { get; set; } = default!;
+        public Region Region { get; set; } = default!;
+    }
+}
